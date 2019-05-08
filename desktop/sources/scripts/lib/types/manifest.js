c5d53681473f7daf66b4927ad0bcf68ff08abade
Ryan Ramage <ryan.ramage@gmail.com>
2019-05-07 22:58:48 -0600

    goodbye desktop

--- desktop/sources/scripts/lib/types/manifest.js
+++
@@ -1,7 +0,0 @@
-const Joi = require('joi-browser')
-
-module.exports = {
-  title: Joi.string().description('the title of the project'),
-  artistId: Joi.string().description('the artistId who created this project'),
-  viz: Joi.array().items(Joi.string())
-}
--- desktop/sources/scripts/udp/routes/index.js
+++
@@ -1,8 +0,0 @@
-// keep sorted with highest priority at the top
-module.exports = [
-    require('./play')
-  , require('./volume')
-  , require('./mute')
-  , require('./solo')
-  , require('./pan')
-]
